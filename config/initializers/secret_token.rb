2642c30f49f742edd219caefe3a1218dfc0bc83c
Theo Pack <tf.pack@gmail.com>
2013-05-25 00:54:32 +0200

    inital import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FuriBlog::Application.config.secret_token = 'c36bc59ec4a44dd0a6af9619ce861c096a509c68f2dca58f1b564e91230a605b589c8175391668c6973739827ed2cf53b6b1f64a3a9200c3aebcf2d349bc4204'
